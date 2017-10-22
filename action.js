c9006b79a1702c439c71fa3ded364722c7e5c308
Omari Bey <beyor@g.cofc.edu>
2017-10-22 13:40:52 -0400

    lab part 2

--- action.js
+++ action.js
@@ -1,15 +1,30 @@
 function checkEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    var result = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return result.test(email);
+
 
 }
 
 function checkAge() {
     var regEx = new RegExp('\w');
     
-    if() {
-       
+    if(checkAge < 100 ) {
+       return true;
     } else {
-       
+       return confirm("not valid");
     }
 }
+
+function validate() {
+    ("#result").text("");
+    var email = ("#email").re();
+    if (checkEmail(email)) {
+        ("#result").text(email+ " is valid");
+        ("#result").css("color", "green");
+    } else {
+        ("#result").text(email + "is not valid");
+        ("#result").css("color", "red");
+    }
+    return false;
+}
+("#validate").bind("Subscribe", validate);
